0ef922cef902b7a4ea31cfff516d67850e075a07
Sefa Demir <sefa58demir@gmail.com>
2022-09-01 23:16:00 +0300

    ilk commit

---
+++ ornek.js
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+	<meta http-equiv="content - type content = "text/htmml;charset=utf-8>
+<title> Sefa Deneme </title>
+
+</head>
+<body>
+	<script type="text/javascript" language="javascript">
+
+var degisimvar = 5 ;
+var degisimyok = "5" ;
+
+document.write degisimvar + "<br />" ;
+
+document.write (typeof(degisimvar) + ":" + degisimvar ); 
+		
+
+	</script>
+</body> 
+
+</html>
